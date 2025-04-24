a8f80cf6bc541f6c6eaf8e504ff9277491daa403
UselessHumster <79360700+UselessHumster@users.noreply.github.com>
2025-04-24 16:35:55 +0300

    Реализовал игру prime

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -12,9 +12,13 @@ from brain_games.games.brain_gcd import RULE as GDC_RULE
 from brain_games.games.brain_progression import game as progression_game
 from brain_games.games.brain_progression import RULE as PROGRESSION_RULE
 
+from brain_games.games.brain_prime import game as prime_game
+from brain_games.games.brain_prime import RULE as PRIME_RULE
+
 
 __all__ = ['start_game',
            'calc_game', 'CALC_RULE',
            'even_game','EVEN_RULE',
            'gcd_game','GDC_RULE',
-           'progression_game','PROGRESSION_RULE']
+           'progression_game','PROGRESSION_RULE',
+           'prime_game', 'PRIME_RULE']
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,5 @@
+from brain_games.games import start_game, prime_game, PRIME_RULE
+
+
+if __name__ == '__main__':
+    start_game(prime_game, PRIME_RULE)
